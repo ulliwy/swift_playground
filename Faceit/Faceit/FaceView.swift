0f8545a1f60867ec21d5148a8399c517634b2014
ulliwy <inessa.t@icloud.com>
2018-04-13 15:36:38 -0700

    Faceit app

---
+++ Faceit/Faceit/FaceView.swift
@@ -0,0 +1,112 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by Inessa Turovtseva on 4/10/18.
+//  Copyright © 2018 IP. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class FaceView: UIView {
+    
+    @IBInspectable
+    var scale: CGFloat = 0.9
+    
+    @IBInspectable
+    var eyesOpen: Bool = true
+    
+    @IBInspectable
+    var mouthCurvature: Double = 0.2 // 1.0 full smile, -1.0 full down
+    
+    @IBInspectable
+    var lineWidth: CGFloat = 5.0
+    
+    @IBInspectable
+    var color: UIColor = UIColor.blue
+    
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private enum Eye {
+        case left
+        case right
+    }
+    
+    private func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadius / Ratio.skullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratio.skullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratio.skullRadiusToMouthOffset
+        
+        let mouthRect = CGRect(
+            x: skullCenter.x - mouthWidth / 2,
+            y: skullCenter.y + mouthOffset,
+            width: mouthWidth,
+            height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+        
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 3, y: start.y + smileOffset)
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 3, y: start.y + smileOffset)
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = lineWidth
+        
+        return path
+    }
+    
+    private func pathForEye(_ eye: Eye) -> UIBezierPath {
+        func centerOfEye(_ eye: Eye) -> CGPoint {
+            let eyeOffset = skullRadius / Ratio.skullRatioToEyeOffset
+            var eyeCenter = skullCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+            return eyeCenter
+        }
+        
+        let eyeRadius = skullRadius / Ratio.skullRadiusToEyeRadius
+        let eyeCenter = centerOfEye(eye)
+        let path: UIBezierPath
+        if eyesOpen {
+            path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        } else {
+            path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat.pi, clockwise: true)
+        }
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: false)
+        path.lineWidth = lineWidth
+        return path
+    }
+
+    override func draw(_ rect: CGRect) {
+        color.set()
+        pathForSkull().stroke()
+        pathForEye(.left).stroke()
+        pathForEye(.right).stroke()
+        pathForMouth().stroke()
+    }
+    
+    private struct Ratio {
+        static let skullRatioToEyeOffset: CGFloat = 3
+        static let skullRadiusToEyeRadius: CGFloat = 10
+        static let skullRadiusToMouthWidth: CGFloat = 1
+        static let skullRadiusToMouthHeight: CGFloat = 3
+        static let skullRadiusToMouthOffset: CGFloat = 4
+    }
+
+}
---
+++ Faceit/Faceit/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Inessa Turovtseva on 4/10/18.
+//  Copyright © 2018 IP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
